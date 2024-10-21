4103c77c78cde2106a366dd0b3d75ecd86d0cc27
zulja1 <luka.zuljo@gmail.com>
2024-10-21 19:03:34 +0200

    intital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <header>
+            <h1>Mosa gej</h1>
+        </header>
+    )
+}
+
+export default Header;
