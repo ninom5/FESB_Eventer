8b06dfb65cd9d44fb98038dc55df80630320e01c
ninom5 <nmilas00@fesb.hr>
2024-10-22 21:05:51 +0200

    testovi

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,14 +1,73 @@
+import "../styles/loginPage.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const [registerValues, setRegisterValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegisterValues({ ...registerValues, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/login",
+        registerValues
+      );
+      if (response.data === "Successfully logged in") {
+        navigate("/home");
+      } else if (response.data === "Server error") alert("server error");
+      else if (response.data === "Wrong email or password")
+        alert("wrong email or password");
+      else {
+        console.error("Login failed");
+      }
+    } catch (err) {
+      console.error("Error during login:", err);
+    }
+  };
+
   return (
-    <div>
+    <div className="login-container">
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      <div className="login">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <FontAwesomeIcon icon={faUser} />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={registerValues.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-group">
+            <FontAwesomeIcon icon={faLock} />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={registerValues.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
-}   
+}
 
 export default LoginPage;
