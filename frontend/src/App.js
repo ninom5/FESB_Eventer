5b5b88720c469737cfbd3e71973e298d1c97bb64
Ivan Mosic <ivan.mosic7@gmail.com>
2025-01-20 13:46:40 +0100

    Merge malih promjena na main-u

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,49 +7,57 @@ import ProtectedRoute from "../src/ProtectedRoute";
 import ProfilePage from "./pages/ProfilePage";
 import EventsPage from "./pages/EventsPage";
 import MyEventsPage from "./pages/MyEventsPage";
+import { LoadScript } from "@react-google-maps/api";
+
+const libraries = ["places"];
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+      libraries={libraries}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
 
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <ProfilePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/events"
-          element={
-            <ProtectedRoute>
-              <EventsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/myevents"
-          element={
-            <ProtectedRoute>
-              <MyEventsPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path="/profile/:email"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/events"
+            element={
+              <ProtectedRoute>
+                <EventsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/myevents"
+            element={
+              <ProtectedRoute>
+                <MyEventsPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </LoadScript>
   );
 }
 
--- frontend/src/components/EventsPage/EventForm.jsx
+++ frontend/src/components/EventsPage/EventForm.jsx
@@ -112,6 +112,7 @@ function EventForm({
           <Autocomplete
             onLoad={handleAutocompleteLoad}
             options={{ componentRestrictions: { country: "HR" } }}
+            value={eventLocation}
             onPlaceChanged={handlePlaceChanged}
           >
             <input
